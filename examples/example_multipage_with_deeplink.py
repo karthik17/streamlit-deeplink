ea453339faeb91789f444408736a24ef652824ee
karthik17 <karthe@gmail.com>
2023-03-08 09:12:50 -0600

    format changes

--- examples/example_multipage_with_deeplink.py
+++ examples/example_multipage_with_deeplink.py
@@ -1,10 +1,10 @@
 import streamlit as st
 from st_deeplink import deep_link
 
+
 def main_page():
     st.markdown("# Main page 🎈")
     st.sidebar.markdown("# Main page 🎈")
-
 
 def page2():
     st.markdown("# Page 2 ❄️")
@@ -14,7 +14,6 @@ def page2():
         ('Email', 'Home phone', 'Mobile phone'),
         key='contact_method')
     st.write('You selected:', option)
-
 
 def page3():
     st.markdown("# Page 3 🎉")
@@ -26,16 +25,16 @@ def page3():
         key='colors')
     st.write('You selected:', options)
 
-
 page_names_to_funcs = {
     "Main Page": main_page,
     "Page 2": page2,
     "Page 3": page3,
 }
 
-
 def run():
-    selected_page = deep_link(st.sidebar.selectbox)("Select a page", page_names_to_funcs.keys(), key='page')
+    selected_page = deep_link(st.sidebar.selectbox)("Select a page",
+        page_names_to_funcs.keys(),
+        key='page')
     page_names_to_funcs[selected_page]()
 
 
